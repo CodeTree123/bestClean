9ef6719988748a6d01d7ee234b34982683128e1d
CodeTree123 <codetree.developers@gmail.com>
2022-06-05 16:56:56 +0600

    hotline added, contact form made

---
+++ mailsend.js
@@ -0,0 +1,25 @@
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('c_MHFJI2mM4YDt0Du');
+})(); 
+
+
+const btn = document.getElementById('button');
+
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  btn.value = 'Sending...';
+
+  const serviceID = 'default_service';
+  const templateID = 'template_jt0zjsk';
+
+  emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+}); 
